ec5f03d659f4a40b4226115ccd1914b76a26166f
Bablu Kumar <bkrockx@gmail.com>
2017-10-26 01:31:32 +0530

    Update Response.java

--- src/main/java/com/phonepe/model/Response.java
+++ src/main/java/com/phonepe/model/Response.java
@@ -3,10 +3,7 @@ package com.phonepe.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-/**
- * @author chintu.kumar
- * @date 10/16/17
- */
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response {
     @JsonProperty("message")
